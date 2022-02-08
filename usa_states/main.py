0afd966848b4ec9cb79943e01403e69ebf403dad
Manoel Britto <manoelbritto@gmail.com>
2022-02-07 21:23:40 -0500

    fixing code

--- usa_states/main.py
+++ usa_states/main.py
@@ -14,9 +14,6 @@ new_turtle.hideturtle()
 # read csv with Pandas
 location = pd.read_csv("50_states.csv")
 
-# pop-up start a text box
-answer_state = screen.textinput(title="Type a State", prompt="Type another state's name:")
-
 def setposition(x, y, state):
     new_turtle.penup()
     new_turtle.goto(x, y)
@@ -25,19 +22,20 @@ def setposition(x, y, state):
 def settitle(title):
     return screen.textinput(title=title, prompt="Type another state's name:")
 
-# print(f"{int(alabama_state['x'])}, {int(alabama_state['y'])}")
-# setposition(int(alabama_state['x']), int(alabama_state['y']), "Alabama")
-
 score = 0
+title_new="Type a State"
+# pop-up start a text box
+answer_state = screen.textinput(title=title_new, prompt="Type another state's name:")
 
 while score < 50:
-    state_value = location[location["state"].str.upper() == answer_state.upper()]
+    state_value = location[location["state"].str.upper() == settitle(title_new).upper()]
     if len(state_value) > 0:
         score += 1
         setposition(int(state_value['x']), int(state_value['y']), str(state_value["state"]))
         title_new = f"{score} of 50"
-        settitle(title_new)
 
+    if score == 0:
+        title_new = 0
 turtle.mainloop()
 
 # get coordination from any image
